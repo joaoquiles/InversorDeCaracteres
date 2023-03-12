937d3c2d98aa391fd543d69ea30b522cbc2f8486
João Victor Santana Quiles <42985671+joaoquiles@users.noreply.github.com>
2023-03-12 16:46:03 -0300

    Add files via upload

---
+++ InversorDeCaracteres/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str, invertida = "";
+        int i;
+
+        Console.Write("Digite uma string: ");
+        str = Console.ReadLine();
+
+        for (i = str.Length - 1; i >= 0; i--)
+        {
+            invertida += str[i];
+        }
+
+        Console.WriteLine("String invertida: {0}", invertida);
+    }
+}
